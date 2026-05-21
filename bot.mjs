ae63f375cc4b85ea69211be4e7542908dc3ddbf0
Quoc Bao Nguyen <3flowers2004@gmail.com>
2026-05-21 19:07:10 +0700

    finish the bot

--- bot.mjs
+++ bot.mjs
@@ -22,11 +22,11 @@ async function replyFromGemini(userText, isGroup){
     contents: userText,
     config: {
       systemInstruction: isGroup ? "You are a helpful bot in a Zalo group chat. Just reply in 1-2 sentences"
-      : "You are a helpful bo:workingt in a Zalo private chat, Just reply in 1-2 sentences",
+      : "You are a helpful bot in a Zalo private chat, Just reply in 1-2 sentences",
     },
   });
   
-  const text = response?.trim(); // check if respone is not null then delete the space on start and end of respone with trim()
+  const text = response.text?.trim(); // check if respone is not null then delete the space on start and end of respone with trim()
   if(!text) throw new Error("Empty response from Gemini!") // if text is null, throw Error and exit
   return text.length > 2000 ? text.slice(0, 1500) + "(... Tu bi không tình yêu!)" : text; // check if the response is too long, it will keep from 0 to 1500 word and add ... in the end to avoid banned from Zalo.
 }
@@ -34,4 +34,35 @@ async function replyFromGemini(userText, isGroup){
 // login to zalo
 const zalo = new Zalo();
 const api = await zalo.loginQR(); // the object api will handle all things after
-// login via QR need a moment
+
+// register an event handle when a message coming
+// (message) => will run every time when a message arrive
+api.listener.on("message", (message) => { // "message" is event when someone send message
+  // only reply to text message from others
+  const isPlainText = typeof message.data.content === "string";
+  if (message.isSelf || !isPlainText) return;
+
+  //prepare data fpr Gemini and for sending back
+  const userText = message.data.content;
+  const threadType = message.type;
+  const isGroup = threadType === ThreadType.Group;
+  
+  void (async () => {
+    try {
+      const reply = await replyFromGemini(userText, isGroup);
+      await api.sendMessage({msg: reply}, message.threadId, threadType);
+    } catch (err) {
+      // add console write the error to log
+      console.error("Gemini error: ", err);
+      await api.sendMessage(
+        { msg: "Sorry, I couldn't reply right now. Please try again."},
+        message.threadId,
+        ThreadType
+      );
+    }
+  })();
+})
+
+// start listening
+api.listener.start();
+
